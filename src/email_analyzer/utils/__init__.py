dccb203ff10f1d86b97cfa27a550642dcca7f9c4
Алексей Нефедов <ngpless@mail.fr>
2026-04-19 17:02:06 +0300

    refactor(utils): re-export csv/json/logging helpers from package __init__

--- src/email_analyzer/utils/__init__.py
+++ src/email_analyzer/utils/__init__.py
@@ -1,14 +1,17 @@
-"""Вспомогательные утилиты: аутентификация, экспорт документов."""
+"""Вспомогательные утилиты: аутентификация, экспорты, логирование."""
 
-from email_analyzer.utils.security import (
-    hash_password,
-    verify_password,
-    create_access_token,
-    decode_access_token,
-)
+from email_analyzer.utils.csv_export import export_emails_to_csv
+from email_analyzer.utils.json_export import export_emails_to_json
+from email_analyzer.utils.logging_setup import configure, get_logger
 from email_analyzer.utils.reports import (
     export_emails_to_docx,
     export_emails_to_xlsx,
+)
+from email_analyzer.utils.security import (
+    create_access_token,
+    decode_access_token,
+    hash_password,
+    verify_password,
 )
 
 __all__ = [
@@ -18,4 +21,8 @@ __all__ = [
     "decode_access_token",
     "export_emails_to_docx",
     "export_emails_to_xlsx",
+    "export_emails_to_csv",
+    "export_emails_to_json",
+    "configure",
+    "get_logger",
 ]
